32e4e8e922aadae592b759347b05690e5f98564e
eryc_s.z.w <erycszw10@gmail.com>
2024-12-02 23:41:40 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+
+  return (
+    <>
+      <div className="font-playfair text-red-600 font-semibold text-3xl">Hello this is shelf app</div>
+      <p className="text-2xl font-nunito">this is a paragraph</p>
+    </>
+    
+  )
+}
+
+export default App
